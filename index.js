65cf3a63fcd1580bc1b87a654e59e200cdc9c7ab
Abdul-bari-Zahid <bariblogger6@gmail.com>
2026-03-03 04:22:41 +0500

    final

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://student-panel-frontend-sigma.vercel.app',
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 // allow larger JSON bodies for base64 image uploads
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
